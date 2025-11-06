ad4d6ebe7730aec7ad5196807100670ab4e36c72
milanterhes <terhes.milan@gmail.com>
2025-11-06 16:43:28 +0100

    wip

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,13 +1,5 @@
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  const traceExporter = new OTLPTraceExporter({
-    url: `https://${process.env.INGESTING_HOST}/v1/traces`,
-    headers: {
-      Authorization: `Bearer ${process.env.SOURCE_TOKEN}`,
-    },
-  });
-
-  registerOTel({ serviceName: "werkstatt", traceExporter });
+  registerOTel({ serviceName: "werkstatt" });
 }
